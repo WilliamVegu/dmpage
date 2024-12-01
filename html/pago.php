f97a55111dde7c4d044d4dbbbf82e2367eb8e427
WilliamVegu <136489536+WilliamVegu@users.noreply.github.com>
2024-12-01 14:45:27 -0500

    Add files via upload

---
+++ html/pago.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    echo "<script>
+        alert('Por favor, inicie sesión primero.');
+        window.location.href = '/html/registro.php';
+    </script>";
+    exit;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Método de Pago</title>
+</head>
+<body>
+<header>
+        <nav>
+            <ul>
+            <li><a href="main.php">Admisión Smart</a></li>
+                <li><a href="registro.php">Registrarse/Iniciar sesión</a></li>
+                <li><a href="#nosotros">Nosotros</a></li>
+                <li><a href="#planes">Planes</a></li>
+                <li><a href="pago.php">Pago</a></li>
+                <li><a href="usuario.php">Usuario</a></li>
+                
+            </ul>
+        </nav>
+    </header>
+    <h1 style="text-align: center;">METODO DE PAGO</h1>
+    
+    <div>
+        <h2>CODIGO QR DE PAGO</h2>
+        <h3>YAPE</h3>
+        
+        <div>
+            <img src="/img/qr.png" alt="QR de Pago">
+        </div>
+        <br>
+            <a href="/img/Instructivo.pdf" download>Seleccione AQUÍ si tiene dudas </a>
+        
+        <form action="/php/pago.php" method="POST" >
+            <label for="codigo">CÓDIGO DE OPERACIÓN:</label>
+            <input type="text" id="codigo" name="codigo" required><br><br>
+            <label for="numero">Número de teléfono con el que se realizará el yapeo:</label>
+            <input type="tel" id="numero" name="numero" required pattern="^\+?[0-9]{1,3}?[ -]?[0-9]{6,12}$" placeholder="+34 123 456 789"><br><br>
+            <button type="submit">ENVIAR</button>
+        </form> 
+
+    </div>
+    <script src="/js/pago.js"></script>
+</body>
+</html>
---
+++ php/compr.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+require '..\php\conexion.php';
+// Verificar que el formulario fue enviado con el método POST
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Obtener datos del formulario
+        $user = $_POST['correo'];
+        $pass = $_POST['contrase'];
+
+        try {
+            // Preparar la consulta SQL
+            $sql = "SELECT * FROM usuario WHERE correo = :correo AND contrase = :contrase";
+            $stmt = $pdo->prepare($sql);
+
+            // Enlazar parámetros
+            $stmt->bindParam(':correo', $user, PDO::PARAM_STR);
+            $stmt->bindParam(':contrase', $pass, PDO::PARAM_STR);
+
+            // Ejecutar la consulta
+            $stmt->execute();
+
+            // Verificar si el usuario existe
+            if ($stmt->rowCount() > 0) {
+                $_SESSION['username'] = $user; // Almacenar el nombre de usuario en la sesión
+                echo "<script>
+        window.location.href = '../html/usuario.php';
+    </script>";
+            } else {
+                echo "<script>
+        alert('Usuario o contraseña incorrectos');
+        window.location.href = '/html/registro.php';
+    </script>";
+            }
+        } catch (PDOException $e) {
+            // Manejar el error de la consulta
+            error_log("Error en la consulta: " . $e->getMessage()); // Registra el error en el archivo de log del servidor
+            die("Error en la consulta: " . $e->getMessage());
+        }
+    } else {
+        die("Método de solicitud no válido. Por favor, use el método POST.");
+    }
