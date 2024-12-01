6d3a729771b9082fd1dd4d20339a971b0ce13b4a
WilliamVegu <136489536+WilliamVegu@users.noreply.github.com>
2024-12-01 14:44:38 -0500

    Delete pagina_dm directory

--- pagina_dm/js/pago.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('beforeunload', function (event) {
-    // Si deseas bloquear la salida, establece un mensaje.
-    event.preventDefault();
-    event.returnValue = ''; // Necesario para que funcione en algunos navegadores.
-});
